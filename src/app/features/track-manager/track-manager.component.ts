8c51885d572317485151fc9b1f4407e0baefd090
MohamedMoustir <itsmoustir@gmail.com>
2026-01-07 11:27:37 +0100

    feat: update track manager component logic

--- src/app/features/track-manager/track-manager.component.ts
+++ src/app/features/track-manager/track-manager.component.ts
@@ -3,7 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router'
 import { TrackService } from '../../core/services/track.service';
-import { MusicCategory } from '../../core/models/track';
+import { CreateTrackDTO, MusicCategory } from '../../core/models/track';
 import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-track-manager',
@@ -16,11 +16,12 @@ export class TrackManagerComponent {
   private router = inject(Router);
 
   protected trackService = inject(TrackService);
+ 
 
   categories: MusicCategory[] = ['Pop', 'Rock', 'Rap', 'Jazz', 'Classical', 'Electro', 'Other'];
 
   selectedAudioFile: File | null = null;
-  selectedCouverFile: File | null = null;
+  selectedCoverFile: File | null = null;
 
   trackForm = this.fb.group({
     title: ['', [Validators.required, Validators.maxLength(50)]],
@@ -29,7 +30,7 @@ export class TrackManagerComponent {
     description: ['', [Validators.maxLength(200)]]
   });
 
-  onAudiFileSelected(event: Event): void {
+  onAudioFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.selectedAudioFile = input.files[0];
@@ -38,9 +39,36 @@ export class TrackManagerComponent {
   onCoverSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
-      this.selectedCouverFile = input.files[0];
+      this.selectedCoverFile = input.files[0];
     }
   }
-  
 
+  async onSubmit(): Promise<void> {
+    if (this.trackForm.invalid || !this.selectedAudioFile) {
+      this.trackForm.markAllAsTouched();
+      return;
+    }
+
+    const formValue = this.trackForm.value;
+    const metadata: CreateTrackDTO = {
+      title: formValue.title!,
+      artist: formValue.artist!,
+      category: formValue.category as MusicCategory,
+      description: formValue.description || '',
+      cover: this.selectedCoverFile || undefined
+
+    }
+
+    try {
+      await this.trackService.addTrack(this.selectedAudioFile, metadata);
+      if (this.trackService.status() === 'success') {
+        this.router.navigate(['/library']);
+      }
+    } catch (error) {
+
+    }
+
+
+  }
+ 
 }
