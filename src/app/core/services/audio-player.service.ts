618f0dfdbb6e4f247c3b1d5a26d99ef8e81e20df
MohamedMoustir <itsmoustir@gmail.com>
2026-01-07 11:27:33 +0100

    feat: enhance audio player service controls

--- src/app/core/services/audio-player.service.ts
+++ src/app/core/services/audio-player.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Track } from '../models/track';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AudioPlayerService {
 
-  constructor() { }
+  private audio = new Audio();
+  currentTrack = signal<Track | null>(null);
+  isPlaying = signal<boolean>(false);
+
+  playTrack(track: Track) {
+    if (this.currentTrack()?.id === track.id) {
+      this.togglePlay();
+      return;
+    }
+    this.currentTrack.set(track);
+    const fileUrl = URL.createObjectURL(track.file);
+    this.audio.src = fileUrl;
+    this.audio.load();
+    this.play();
+  }
+
+  togglePlay() {
+    if (this.audio.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+
+  private play(){
+    this.audio.play()
+    .then(()=>this.isPlaying.set(true))
+    .catch(err=>console.log('Error playing audio:', err));
+    
+  }
+
+  private pause(){
+    this.audio.pause();
+    this.isPlaying.set(false);
+  }
 }
