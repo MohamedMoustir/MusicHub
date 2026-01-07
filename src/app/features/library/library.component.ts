fb19b91143352573a8c7e7f45301b80d8fcc966a
MohamedMoustir <itsmoustir@gmail.com>
2026-01-07 11:27:50 +0100

    feat: implement library component functionality

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormatTimePipe } from '../../shared/pipes/format-time.pipe';
+import { TrackService } from '../../core/services/track.service';
+import { AudioPlayerService } from '../../core/services/audio-player.service';
 
 @Component({
   selector: 'app-library',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormatTimePipe],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
 export class LibraryComponent {
 
+  private trackService = inject(TrackService);
+  tracks$ = this.trackService.getAllTracks();
+  playerService = inject(AudioPlayerService)
+
+   onPlay(track: any) {
+    console.log('✅ Clicked Track:', track);
+  console.log('📁 File Type:', track.file instanceof Blob);
+    this.playerService.playTrack(track);
+  }
+  async deleteTrack(id: number, event: Event) {
+    event.stopPropagation();
+    if (confirm('Voulez-vous vraiment supprimer ce morceau ?')) {
+      await this.trackService.deleteTrack(id);
+    }
+  }
 }
