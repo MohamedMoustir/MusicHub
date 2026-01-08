06fa2bf6dff28c466580b0ee48742bbc6fc1581a
MohamedMoustir <itsmoustir@gmail.com>
2026-01-08 11:55:37 +0100

    feat(routing): register routes for track editing and details view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
+import { TrackManagerComponent } from './features/track-manager/track-manager.component';
+import { LibraryComponent } from './features/library/library.component';
 
 export const routes: Routes = [
-    { path: '',redirectTo:'add-track',pathMatch:'full' },
-    {
-        path:'add-track',
-        loadComponent:()=>import('./features/track-manager/track-manager.component').then(m=>m.TrackManagerComponent)
-    },
-    {
-        path:'library',
-        loadComponent:()=>import('./features/library/library.component').then(m=>m.LibraryComponent)
-    }
+    { path: '', redirectTo: 'add-track', pathMatch: 'full' },
+    { path: 'add-track', component: TrackManagerComponent },
+    { path: 'library', component: LibraryComponent },
+    { path: 'edit-track/:id', component: TrackManagerComponent },
+    { path: 'track/:id', component: TrackManagerComponent },
+    { path: '**', redirectTo: 'library' }
 
 
 ];
